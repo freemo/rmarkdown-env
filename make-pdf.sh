b31159336e05e43e1bef551ee09dd86b8b66fd15
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2017-04-20 18:17:42 -0400

    added inline graphviz to syncleus-white.

--- make-pdf.sh
+++ make-pdf.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 for file in *.Rmd
 do
- # do something on $file
  Rscript -e "library(rmarkdown); rmarkdown::render('$file', 'pdf_document')" 
 done
+
+pdflatex --shell-escape syncleus-white-example.tex
+
+#for file in *.tex
+#do
+# pdflatex --shell-escape "$file" 
+#done
+
