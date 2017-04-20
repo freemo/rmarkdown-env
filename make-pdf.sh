d45bb087e4498ae1fb271c36e8da78d1692bbed7
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2017-04-20 18:39:06 -0400

    Updated the other templates to include inline graphviz support.

--- make-pdf.sh
+++ make-pdf.sh
@@ -4,10 +4,8 @@ do
  Rscript -e "library(rmarkdown); rmarkdown::render('$file', 'pdf_document')" 
 done
 
-pdflatex --shell-escape syncleus-white-example.tex
-
-#for file in *.tex
-#do
-# pdflatex --shell-escape "$file" 
-#done
+for file in *.tex
+do
+ pdflatex --shell-escape "$file" 
+done
 
